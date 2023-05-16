574b83c2751f0cc732adb8ffdd2069f83af3e646
amarko1 <amarko1@racunarstvo.hr>
2023-05-16 16:45:30 +0200

    Settings novi

--- WindowsForms/Forms/Settings.Designer.cs
+++ WindowsForms/Forms/Settings.Designer.cs
@@ -28,34 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLanguage = new System.Windows.Forms.Button();
-            this.btnGender = new System.Windows.Forms.Button();
             this.lblSettings = new System.Windows.Forms.Label();
+            this.cbLanguage = new System.Windows.Forms.ComboBox();
+            this.lbLanguage = new System.Windows.Forms.Label();
+            this.lbGender = new System.Windows.Forms.Label();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnLanguage
-            // 
-            this.btnLanguage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.btnLanguage.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnLanguage.ForeColor = System.Drawing.Color.White;
-            this.btnLanguage.Location = new System.Drawing.Point(117, 163);
-            this.btnLanguage.Name = "btnLanguage";
-            this.btnLanguage.Size = new System.Drawing.Size(178, 116);
-            this.btnLanguage.TabIndex = 0;
-            this.btnLanguage.Text = "Language";
-            this.btnLanguage.UseVisualStyleBackColor = false;
-            // 
-            // btnGender
-            // 
-            this.btnGender.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.btnGender.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnGender.ForeColor = System.Drawing.Color.White;
-            this.btnGender.Location = new System.Drawing.Point(481, 163);
-            this.btnGender.Name = "btnGender";
-            this.btnGender.Size = new System.Drawing.Size(178, 116);
-            this.btnGender.TabIndex = 1;
-            this.btnGender.Text = "Gender";
-            this.btnGender.UseVisualStyleBackColor = false;
             // 
             // lblSettings
             // 
@@ -64,19 +43,71 @@
             this.lblSettings.ForeColor = System.Drawing.Color.White;
             this.lblSettings.Location = new System.Drawing.Point(325, 67);
             this.lblSettings.Name = "lblSettings";
-            this.lblSettings.Size = new System.Drawing.Size(122, 37);
+            this.lblSettings.Size = new System.Drawing.Size(150, 46);
             this.lblSettings.TabIndex = 2;
             this.lblSettings.Text = "Settings";
             // 
+            // cbLanguage
+            // 
+            this.cbLanguage.FormattingEnabled = true;
+            this.cbLanguage.Location = new System.Drawing.Point(119, 168);
+            this.cbLanguage.Name = "cbLanguage";
+            this.cbLanguage.Size = new System.Drawing.Size(184, 28);
+            this.cbLanguage.TabIndex = 3;
+            // 
+            // lbLanguage
+            // 
+            this.lbLanguage.AutoSize = true;
+            this.lbLanguage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbLanguage.Location = new System.Drawing.Point(119, 136);
+            this.lbLanguage.Name = "lbLanguage";
+            this.lbLanguage.Size = new System.Drawing.Size(97, 28);
+            this.lbLanguage.TabIndex = 4;
+            this.lbLanguage.Text = "Language";
+            // 
+            // lbGender
+            // 
+            this.lbGender.AutoSize = true;
+            this.lbGender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbGender.Location = new System.Drawing.Point(484, 137);
+            this.lbGender.Name = "lbGender";
+            this.lbGender.Size = new System.Drawing.Size(76, 28);
+            this.lbGender.TabIndex = 6;
+            this.lbGender.Text = "Gender";
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Location = new System.Drawing.Point(484, 168);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(191, 28);
+            this.cbGender.TabIndex = 5;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(294, 260);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(191, 78);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
             // Settings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(800, 446);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lbGender);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.lbLanguage);
+            this.Controls.Add(this.cbLanguage);
             this.Controls.Add(this.lblSettings);
-            this.Controls.Add(this.btnGender);
-            this.Controls.Add(this.btnLanguage);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Name = "Settings";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -87,9 +118,11 @@
         }
 
         #endregion
-
-        private Button btnLanguage;
-        private Button btnGender;
         private Label lblSettings;
+        private ComboBox cbLanguage;
+        private Label lbLanguage;
+        private Label lbGender;
+        private ComboBox cbGender;
+        private Button btnSubmit;
     }
 }
--- WindowsForms/Forms/Settings.cs
+++ WindowsForms/Forms/Settings.cs
@@ -15,6 +15,56 @@ namespace WindowsForms.Forms
         public Settings()
         {
             InitializeComponent();
+
+            InitializeComboBox();
         }
+
+        private void InitializeComboBox()
+        {
+            cbGender.Items.Add("Men");
+            cbGender.Items.Add("Women");
+
+            cbLanguage.Items.Add("Croatian");
+            cbLanguage.Items.Add("English");
+
+            //default 
+            cbGender.SelectedIndex = 0;
+            cbLanguage.SelectedIndex = 0;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                // spremi u file i idi na iducu formu!
+                //SaveFile();
+            }
+            else
+            {
+                // koristiti u sljedecem ucitavanju vec postojeci file i ne ucitaj formu
+            }
+        }
+
+        private void SaveFile()
+        {
+            string? choosenLanguage = cbLanguage.SelectedItem.ToString();
+            string? choosenGender = cbGender.SelectedItem.ToString();
+
+            string path = "odabrana_reprezentacija.txt";
+            WriteLinesToFile(path, choosenLanguage, choosenGender);
+
+            if (File.Exists(path))
+            {
+                // ne otvaraj settings formu vec onu koja treba ici iza nje
+            }
+        }
+
+        public void WriteLinesToFile(string filePath, string language, string gender)
+        {
+            string[] lines = { language, gender };
+            File.WriteAllLines(filePath, lines);
+        }
+
     }
 }
